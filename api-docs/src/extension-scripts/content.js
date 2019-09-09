b25cc5ece79ec20ad011abf54de24f2c1ff2b14a
vikrant <Vikrant.Kandgaonkar@cognizant.com>
2019-09-09 01:46:47 +0100

    Capture spec details on tab switch

--- api-docs/src/extension-scripts/content.js
+++ api-docs/src/extension-scripts/content.js
@@ -1,13 +1,19 @@
-window.onload = function () {
+window.onload = captureSpecDetails
+document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState == "visible") {
+        captureSpecDetails()
+    }
+});
+
+function captureSpecDetails() {
     let githubFileElement = document.querySelector("table.js-file-line-container")
     if (githubFileElement != null) {
         const message = {
             "url": window.location.href,
             "message": "Hello",
-            "yaml":githubFileElement.innerText.trim().replace(/\n\t/g, '\n')
+            "yaml": githubFileElement.innerText.trim().replace(/\n\t/g, '\n')
         };
         console.log("Inside content.js message is ", message)
         chrome.runtime.sendMessage(message)
     }
 }
-
